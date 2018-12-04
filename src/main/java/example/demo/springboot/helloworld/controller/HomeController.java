76fbcb68ce8f74076bb11a55ec429934a95b7715
cwdgit <cwd2015vip@sina.com>
2018-12-04 18:12:40 +0800

    add files

---
+++ src/main/java/example/demo/springboot/helloworld/controller/HomeController.java
@@ -0,0 +1,58 @@
+package example.demo.springboot.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    String home() {
+        //from Test CI/CD
+        String msg = "Hello World-DEVOPS!!!";
+        System.out.println(msg);
+        return msg;
+    }
+
+    @RequestMapping("/health")
+    String health() {
+        System.out.println("Checking the status");
+        return "{\"status\":\"UP\"}";
+    }
+
+    @RequestMapping("/highcpu")
+    String highcpu() {
+        System.out.println("High CPU test case.");
+        highCPU();
+        return "High CPU Test Case!";
+    }
+
+    @RequestMapping("/oom")
+    String oom() {
+        System.out.println("OutOfMemory test case.");
+        try {
+            for (int i = 0; i < 10000000; i++) {
+                byte[] mem = new byte[1024];
+                mem[0] = 0;
+            }
+        } catch(Throwable e) {
+            System.out.println("OOM Error " + e);
+            //throw e;
+        }
+        return "OOM Test Case!";
+    }
+
+    private void highCPU() {
+        for (int i = 0; i < 100; i++) {
+            accumulate(Integer.MAX_VALUE/2);
+        }
+    }
+    private long accumulate(long max) {
+        long ret = 0;
+        for(long i = 1; i <= max; i++) {
+            ret += i;
+        }
+        return ret;
+    }
+
+}
---
+++ src/test/java/example/demo/springboot/helloworld/HomeControllerTest.java
@@ -0,0 +1,48 @@
+package example.demo.springboot.helloworld;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import java.net.URI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoApplication.class, webEnvironment=WebEnvironment.RANDOM_PORT)
+
+public class HomeControllerTest {
+
+    @Value("${local.server.port}")
+    private Integer port;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void helloWorld() {
+        getRequest("/")
+            .assertStatusCode(OK)
+            .assertResponseBody("Hello World-DEVOPS!!!");
+    }
+
+    private HelloWorldResponse getRequest(String uri) {
+        return new HelloWorldResponse(restTemplate.getForEntity(getUri(uri), String.class));
+    }
+
+
+    protected URI getUri(String uri) {
+        return UriComponentsBuilder
+            .newInstance()
+            .scheme("http")
+            .host("localhost")
+            .port(port)
+            .path(uri)
+            .build()
+            .toUri();
+    }
+}
